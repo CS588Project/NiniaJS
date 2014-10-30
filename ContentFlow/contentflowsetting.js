d4468907a49cd77314bcb7fe8b890e6099754d0d
JayChen <jchen@holomic.com>
2014-10-29 20:12:39 -0700

    fix error in contentflowsetting.js

--- ContentFlow/contentflowsetting.js
+++ ContentFlow/contentflowsetting.js
@@ -4,5 +4,5 @@ var h = window.innerHeight;
 $(document).ready(function() {
     var myNewFlow = new ContentFlow('theflow', 
     	{endOpacity:0.5, visibleItems:4, relativeItemPosition:'center top', maxItemHeight:0.5*h, flowSpeedFactor:0.5} ) ;
-    #can use flowSpeedFactor to change the speed
+    //can use flowSpeedFactor to change the speed
 });
